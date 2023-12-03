e7ad6d3023d2a86e092650939751226a1794567f
CoolCaper <31525658+CoolCaper@users.noreply.github.com>
2023-12-03 14:39:07 -0800

    check

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -1,5 +1,5 @@
 //class from play
-class Roman extends Phaser.Scene{
+class level1 extends Phaser.Scene{
     constructor (){
         super('playScene')
     }
@@ -11,10 +11,10 @@ class Roman extends Phaser.Scene{
 
     create(){
         this.add.image(game.config.width, game.config.height, '/assets/moon_background.png');
-        this.platforms = this.physics.add.staticGroup();
-        const platform = this.platforms.create(400, 550, 'platform').setOrigin(0.5, 0.5);
-        this.physics.world.enable(platform);
-        platform.setImmovable(true);
+        //this.platforms = this.physics.add.staticGroup();
+        //const platform = this.platforms.create(400, 550, 'platform').setOrigin(0.5, 0.5);
+        //this.physics.world.enable(platform);
+        //platform.setImmovable(true);
     }
 
     update(){
